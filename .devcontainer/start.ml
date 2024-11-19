f99bdc86ac968ed513204d1082b98003bec0ae92
Marco Maggesi <1809783+maggesi@users.noreply.github.com>
2024-11-19 17:08:00 +0000

    Update to HOL Light version 2024-10-17.

--- .devcontainer/start.ml
+++ .devcontainer/start.ml
@@ -1,12 +1,21 @@
-#use "make.ml";;
+unset_verbose_symbols();;
 
 loadt "update_database.ml";;
 
-let gc_control = Gc.get();;
+let original_gc_control = Gc.get();;
 
-Gc.set {gc_control with
+let severe_gc_control =
+ {original_gc_control with
   Gc.space_overhead = 60;
   Gc.major_heap_increment = 10;
   Gc.max_overhead = 400};;
+
+let set_servere_gc_control() =
+  Gc.set severe_gc_control;;
+
+let unset_servere_gc_control() =
+  Gc.set original_gc_control;;
+
+set_servere_gc_control();;
 
 Gc.compact();;
