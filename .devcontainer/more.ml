fa273312b9e1777758e23cd949d561a111af9562
Marco Maggesi <1809783+maggesi@users.noreply.github.com>
2025-06-05 09:30:18 +0000

    MAJ Checkpoint scripts.

--- .devcontainer/more.ml
+++ .devcontainer/more.ml
@@ -1,3 +1,3 @@
-needs "/workspaces/hol-light-devcontainer/.devcontainer/psearch.ml";;
-needs "/workspaces/hol-light-devcontainer/.devcontainer/more_tactics.ml";;
-needs "/workspaces/hol-light-devcontainer/.devcontainer/start.ml";;
+loadt "/workspaces/hol-light-devcontainer/.devcontainer/psearch.ml";;
+loadt "/workspaces/hol-light-devcontainer/.devcontainer/more_tactics.ml";;
+loadt "/workspaces/hol-light-devcontainer/.devcontainer/start.ml";;
